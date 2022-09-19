72ddb21e05fb85c29961e33ef6a2c4b1093ad032
Júlio <juliocesar.jc585@gmail.com>
2022-09-19 15:02:40 -0300

    Atualização
    Adicionado exercicio 14

---
+++ Exercicios/Programação Orientada a Objetos/Ex.14/Exercicio14/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio14
+{
+    class Aluno
+    {
+        public double Notas;
+       
+
+        public double SomaTotalNotas()
+        {
+            double totalNotas = Notas + Notas + Notas;
+            return totalNotas;
+        }
+        public bool Aprovado()
+        {
+            if (SomaTotalNotas() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double ValorRestante()
+        {
+            return 60.0 - SomaTotalNotas();
+        }
+       
+        public override string ToString()
+        {
+            return SomaTotalNotas().ToString("F2",CultureInfo.InvariantCulture); 
+        }
+    }
+       
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex.14/Exercicio14/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercicio14;
+using System;
+using System.Globalization;
+
+Aluno a = new Aluno();
+for (int i = 0; i <= 2; i++)
+{
+    Console.Write($"Informe a nota do {i + 1}º do aluno: ");
+    a.Notas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+Console.WriteLine("Nota total = " + a, CultureInfo.InvariantCulture);
+if (a.Aprovado())
+{
+    Console.WriteLine("APROVADO");
+}
+else
+{
+    Console.WriteLine("REPROVADO");
+    Console.WriteLine("Faltaram "  + a.ValorRestante().ToString("F2",CultureInfo.InvariantCulture) + " pontos");
+}
