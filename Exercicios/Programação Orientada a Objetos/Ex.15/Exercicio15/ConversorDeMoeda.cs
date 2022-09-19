0d9db25a35fd5df36a2cf6db1b9c4b84cf9759ef
Júlio <juliocesar.jc585@gmail.com>
2022-09-19 16:08:03 -0300

    Add - Exercicio 15
    Adicionado exercicio 15, resolvido utilizando o método de membros estáticos em C#

---
+++ Exercicios/Programação Orientada a Objetos/Ex.15/Exercicio15/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio15
+{
+    internal class ConversorDeMoeda
+    {
+        public static double ValorEmReais;
+
+        public static double Conversao(double valorConversao)
+        {
+            double convDireta = ValorEmReais * valorConversao;
+            double convComIof = ((convDireta * 6) / 100) + convDireta;
+            return convComIof;
+        }
+    }
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex.15/Exercicio15/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercicio15;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Qual é a cotação do dolar HOJE ? Em R$: ");
+            ConversorDeMoeda.ValorEmReais = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dolares serão comprados ? $ ");
+            double qtdDolar= double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double valorFinal = ConversorDeMoeda.Conversao(qtdDolar);
+
+            Console.WriteLine($"Valor a pagar em R$: " + valorFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
