c4bec716d0f5c0438fec995f6949b6c033851026
Júlio <juliocesar.jc585@gmail.com>
2022-09-19 12:50:06 -0300

    Adição de exercicio
    Adicionado 13º exercicio

---
+++ Exercicios/Programação Orientada a Objetos/Ex.13/Exercicio13/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio13
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - ((SalarioBruto * Imposto) / 100);
+            
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (( SalarioBruto * porcentagem) / 100);
+        }
+        public override string ToString()
+        {
+            return Nome + " R$ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex.13/Exercicio13/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exercicio13;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.Write("Informe o nome do funcionário: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Informe o salario bruto (CTPS) do funcionário R$: ");
+            func.SalarioBruto= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Qual a % do imposto descontada de seu salário: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(" FUNCIONÁRIO: " + func);
+
+            Console.Write($"Informe a % de aumento salarial para este més de {func.Nome}: ");
+            double porc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porc);
+
+            Console.WriteLine();
+            Console.WriteLine("ATUALIZAÇÃO DOS DADOS DO FUNCIONÁRIO, APÓS AUMENTO: " + func);
+
+
+            
+        }
+
+    }
+}
