0fd0aa6e571699c81cab8962c484b278dc169f4b
Júlio <juliocesar.jc585@gmail.com>
2022-09-19 11:57:00 -0300

    Adição de exercicios
    * Para esta versão foram adicionadas pasta e subpasta para exercicios voltados a PROGRAMAÇÃO ORIENTADA  A OBJETOS.
    *Foram adicionados Ex.12

---
+++ Exercicios/Programação Orientada a Objetos/Ex.12/Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercicio12;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+            
+            Console.Write("Informe a largura do retângulo: ");
+            r.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Informe a altura do retangulo: ");
+            r.Altura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Área do retângulo: " + r);
+        }
+    }
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex.12/Exercicio12/Exercicio12/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio12
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public override string ToString()
+        {
+            return Area().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
