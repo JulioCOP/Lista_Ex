8da3fada1b46b6d6a65bea7cbb4d3c8a03a3d498
Júlio <juliocesar.jc585@gmail.com>
2022-09-20 11:19:24 -0300

    Novo Projeto
    Projeto de criação de um cadastramento de produtos, para esta versão 1.0 do CadastramentoDeProduto, é possível o usuário informar o nome, quantidade e valor de um produto e se o mesmo deseja adicionar ou remover do estoque uma determinada quantidade. Ao fim retorna o valor total de acordo com a quantidade em estoque.
    
    ** Essa versão contem, atualizações de construtores  e sobrecarga para construtores, além do implemento do comando "this", com intuito de reduzir as linhas de comando da classe Produto

---
+++ CadastramentoDeProduto/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            /*Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());*/
+
+            Produto p = new Produto(nome, preco);
+            // construtor, obriga o usuário a entrar com algum dado, para que não ha valores zerados ou null
+            Produto p2 = new Produto();
+
+            Produto p3 = new Produto { 
+                Nome = "TV", 
+                Preco = 890.70, 
+                Quantidade = 22 }; 
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
