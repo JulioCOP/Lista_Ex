5aaa7abde634f19befc36712008f60ff3aaec69c
Júlio <juliocesar.jc585@gmail.com>
2022-09-20 20:54:50 -0300

    Projeto Conta Bancária
    Programa emula o sistema de um conta bancária, sendo solicitado ao usuário que o mesmo forneça sua conta, nome e se deseja ou não realizar algum deposito.
    
    ** Esta versão 1.0 apresenta um pequeno bug de retorno de dados, ao fim do programa.

---
+++ ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Ex16.ContaBancária
+{
+    class ContaBancaria
+    {
+        //1ºAtributos privados:
+        public int NumeroDaConta { get; private set; }
+        // Usuário pode acessar, mas não pode alterar o número da conta, uma vez que só existe uma conta pra aquele número
+        public string NomeDoTitular { get; set; }
+        // O usuário pode alterar seu nome ao longo da vida
+        public double SaldoDaConta { get; private set; }
+        // Saldo só pode ser alterado por meio de duas alternativas: SALDO OU SAQUE
+
+        // 2º Costrutores:
+        public ContaBancaria(int numero, string titular)
+        {
+            // Atribuições
+            NumeroDaConta = numero;
+            NomeDoTitular = titular;
+        }
+        // Construtor com 3 argumento
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        // this para repassar o construtor de 2 argumentos.
+        {
+            SaldoDaConta = saldo;
+        }
+
+        //COMPILAR
+        public override string ToString()
+        {
+            return "CONTA DO USUÁRIO: " + NumeroDaConta + "NOME DO TITULAR: " + NomeDoTitular + "SALDO DISPONÍVEL R$: " + SaldoDaConta.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+
+
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,58 @@
+﻿using Ex16.ContaBancária;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.Write("Informe o Nº do TITULAR da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            // Loop infinito caso o Nº DA CONTA > 4 digitos
+            while (numero.ToString().Length > 4)
+            {
+                Console.Write("DIITE OS 4 ULTIMOS NÚMEROS DA CONTA: ");
+                numero = int.Parse(Console.ReadLine());
+                if (numero.ToString().Length <= 4)
+                {
+                    break;
+                }
+            }
+            Console.Write("Informe o primeiro e o ultimo nome do TITULAR da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write($"{titular}, você irá fazer algum depósito incial ? [S] / [N] ");
+            char resp = char.Parse(Console.ReadLine());
+            while (resp != 's' && resp != 'S' && resp != 'n' && resp != 'N')
+            // Looping infintio em caso de o usuário informar a resposta errada
+            {
+                Console.Write($"{titular}, você irá fazer algum depósito incial ? [S] / [N] ");
+                resp = char.Parse(Console.ReadLine());
+                if (resp == 's' || resp == 'S' || resp == 'n' || resp == 'N')
+                {
+                    break;
+                }
+            }
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Digite o valor R$: ");
+                // Uso de uma variável, temporária
+                double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new ContaBancaria(numero, titular, deposito);
+                // Construtor para receber os atributos da conta
+
+            }
+            else if (resp == 'n' || resp == 'N')
+            {
+                Console.WriteLine($"{titular} optou por não realizar nenhum deposito");
+                conta = new ContaBancaria(numero, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine(conta);
+        }
+    }
+}
+   
