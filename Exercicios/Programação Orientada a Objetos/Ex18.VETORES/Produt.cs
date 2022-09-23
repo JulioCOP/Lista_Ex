a339dee97880108dced45244de1dd89da58dc6d4
Júlio <juliocesar.jc585@gmail.com>
2022-09-23 11:22:05 -0300

    Adicionado Exs
    Adicionado exercicio 18, de vetores voltados a POO

---
+++ Exercicios/Programação Orientada a Objetos/Ex18.VETORES/Produt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex18.VETORES
+{
+    internal class Produt
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex18.VETORES/Program.cs
@@ -0,0 +1,40 @@
+﻿using Ex18.VETORES;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criar classe
+            int n = int.Parse(Console.ReadLine());
+            // Criar o vetor para ser armazenado dados da classe
+
+            Produt[] vect = new Produt[n];
+            // Vetor do tipo classe criado
+            // Ao adicionar um objeto no vetor é necessário instaciar os objetos e depois inseri-los no vetor
+
+            for (int i=0;i<n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // Instaciar o objeto
+                vect[i]= new Produt { Nome=nome, Preco=preco};  
+                
+                // A partir do for, é criado uma estrutura ao qual o vetor criado de acordo com valor informado, que antes tinha suas posições nulas
+                // é instanciado e o valores adicionados para nome e preço são inserido nas posições dos vetores
+            }
+            double soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                soma+= vect[i].Preco;
+                // for pecorre as posições do produtoe acessa as cassas responsáveis pelo preço, que foram adionado no comando anteriormente informado 
+            }
+            double media = soma / n;
+            Console.WriteLine($"Preço médio igual a R$: {media:F2}", CultureInfo.InvariantCulture);
+        }
+    }
+}
