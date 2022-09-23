859ae0818d99c6b34c452642f9b2b1882ab1d187
Júlio <juliocesar.jc585@gmail.com>
2022-09-23 12:49:08 -0300

    Ex.19 - Versão 1.0
    A dona de um pensionato possui dez quartos para alugar para estudantes,
    sendo esses quartos identificados pelo números 0 a 9.
    
    Faça um programa que inicie com todos os dez quartos vazios e depois
    leia uma quantidade N representando o numero de estudantes que vão
    alugar os quartos (N pode ser de 1 ate 10).Em seguida, registre o aluguel dos
    N estudantes. Para cada registro de aluguel, informar o nome e email do
    estudante, bem como qual dos quartos ele escolheu (de a 0 a 9). Suponha
    que seja escolhido um quarto vago.Ao final, seu programa deve imprimir
    um relatório de todas ocupações do pensionato, por ordem de quarto, conforme o exemplo.

---
+++ Exercicios/Programação Orientada a Objetos/Ex.19/Estudante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._19
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return "Nº QUARTO: " + Quarto + "-> " + Nome + "/ " + " Contato: " + Email;
+        }
+    }
+
+
+ 
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex.19/Program.cs
@@ -0,0 +1,35 @@
+﻿using Ex._19;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Mian(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados: ");
+            int numeroQuartos= int.Parse(Console.ReadLine());
+
+            // Criar vetor do tipo classe cin atributos privados
+            Estudante[] vect = new Estudante[numeroQuartos];
+
+            // Estrutura repetitia FOR -> leitura nome, email e quarto desejado -  instanciação e inserir nas posições do vetor
+
+            for (int i = 0; i < numeroQuartos; i++)
+            {
+                Console.Write("NOME: ");
+                string nome = Console.ReadLine();
+                Console.Write($"EMAIL DE {nome}: ");
+                string email = Console.ReadLine();
+                Console.Write($"Nº DO QUARTO P/ ALUGAR: ");
+                int quarto = int.Parse(Console.ReadLine());
+                // Instaciar e adicionar variaveis as pósições dos vetores
+
+                vect[i]= new Estudante(nome,email,quarto);
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
