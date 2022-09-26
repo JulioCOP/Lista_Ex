586607b5d922240cfff51fba49e7bb8065cabf4d
Júlio Oliveira <juliocesar.jc585@gmail.com>
2022-09-26 17:34:09 -0300

    VersãoBasicaSDP
    Versão inicial de um Sistema De Pedidos, em linguagem C#.
        Programa composto por classes e enumerações ao qual informa ao usuário o código, momento da compra e o status do pagamento de um determinado produto.

---
+++ Exercicios/Programação Orientada a Objetos/Ex.22.SistemaDePagamento/Program.cs
@@ -0,0 +1,26 @@
+﻿using SistemaDePedidos.Entities;
+using SistemaDePedidos.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace SistemaDePedidos
+{
+    class Program
+    {
+        static void Main(string[] atgs)
+        {
+            // Instanciar um pedido
+            Order order = new Order
+            {
+                // Código do produto
+                Id = 12345,
+                // Momento da compra do pedido
+                Moment = DateTime.Now,
+                // Status atual em que se encontra a compra desse pedido
+                Status = OrderEStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+
+        }
+    }
+}
