55fc12e31f7bd6a96684dda8206368b9cb9db593
Júlio <juliocesar.jc585@gmail.com>
2022-09-26 12:31:44 -0300

    Ex.21
    Adicionado exercicio 21 de matriz

---
+++ Exercicios/Programação Orientada a Objetos/Ex21.Matrizes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            // Para usuário digitar n linhas e n colunas:
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                // lê a linha inteira, recorta os pedaços em branco e coloca dentro do vetor valores
+                for (int j =0; j < n; j++)
+                // pecorre os vetor valores, armazenando os valores de acordo com a posição
+                {
+                    mat[i, j] = int.Parse(valores[j]);   
+                }
+            }
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i]);
+            }
+            // Percorrer a matriz inteira:
+            int c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; i++)
+                {
+                    // quantidade de negativos
+                    c++;
+                }
+            }
+            Console.WriteLine($"Numeros negativos: {c}");
+        }
+    }
+}
