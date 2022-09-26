656e187a0bd1de6df53ed74bb34a7a5daf84e3d2
Júlio <juliocesar.jc585@gmail.com>
2022-09-26 11:43:20 -0300

    Exercício20- Cadastramento de Salario
    Adicionado exercício 20, que é um projeto para o usuário fazer o cadastramento e registro de uma determinada quantidade de funcionários e selecionar aquele que receberá um aumento salarial, de acordo com ID  de registro.

---
+++ Exercicios/Programação Orientada a Objetos/Ex20.Listas/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._20
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void AumentoSalario(double aumento)
+        {
+            Salario += (Salario * (aumento / 100));
+        }
+        public override string ToString()
+        {
+            return Id + "-> " + Nome + "-> " +"Salário R$: " + Salario;
+        }
+    }
+}
---
+++ Exercicios/Programação Orientada a Objetos/Ex20.Listas/Program.cs
@@ -0,0 +1,57 @@
+﻿using Ex._20;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Usuário, quantos funcionarios vocÊ deseja registrar nesse sistema: ");
+            int numeroDeFuncs = int.Parse(Console.ReadLine());
+            List<Funcionario> listaDeFuncionarios= new List<Funcionario>();
+            // Função lista para adicionar os dados do funcionário a classe do funcionário
+
+            for (int i=1; i<=numeroDeFuncs; i++)
+            {
+                Console.WriteLine($"FUNCIONÁRIO Nº {i}");
+                Console.Write("ID do funcionário: "); 
+                int idFunc=int.Parse(Console.ReadLine());
+                Console.Write("Nome do funcionário: ");
+                string nomeFunc=Console.ReadLine();
+                Console.Write("Salário do funcionário R$: ");
+                double salarioFunc= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                // Adicionar os item da estrutura for dentro da lista
+                listaDeFuncionarios.Add(new Funcionario(idFunc, nomeFunc, salarioFunc));
+            }
+            Console.Write("Informe a ID do funcionário que terá aumento salarial: ");
+            int idAumento = int.Parse(Console.ReadLine());
+
+            Funcionario func = listaDeFuncionarios.Find(x =>x.Id== idAumento);
+            // A classe funcionario, possui uma variavél chamada 'func'. Esta variavel recebe
+            // uma função, ao qual, é a lista de funcionários criada, esta por suas vez
+            // // irá buscar o id informado no inicio do programa e selecionará o mesmo, para receber o 
+            // respectivo aumento, de acordo com % a ser informada.
+
+            if (func != null)
+            {
+                Console.Write("Informe o aumento que este funcionário terá: ");
+                double aumentoSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.AumentoSalario(aumentoSalario);
+            }
+            else
+            {
+                Console.WriteLine("A ID informada não consta para nenhum funcionário informado pelo usuário");
+            }
+            Console.WriteLine();
+            foreach(Funcionario funcionarioDaLista in listaDeFuncionarios)
+            {
+                Console.WriteLine("DADOS DOS FUNCIONÁRIOS: ");
+                Console.WriteLine(funcionarioDaLista.ToString());
+
+            }
+        }
+    }
+}
